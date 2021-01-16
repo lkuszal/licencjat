9a520554f53d84ebb8ffc0bd9bbeceea542e147a
lkuszal <lkuszal1@gmail.com>
2021-01-16 21:51:54 +0100

    intro

--- letterfreq.py
+++ letterfreq.py
@@ -1,24 +1,32 @@
 def simplify(text):
     output = ""
-    for line in open(text,"r", encoding="utf8").readlines():
-        newline=line.casefold().replace("ą","a").replace("ć","c").replace("ę","e").replace("ł","l").replace("ń","n").replace("ó","o").replace("ś","s").replace("ż","z")
+    for line in open(text, "r", encoding="utf8").readlines():
+        newline = line.casefold().replace("ą", "a").replace("ć", "c").replace("ę", "e").replace("ł", "l").replace(
+            "ń", "n").replace("ó", "o").replace("ś", "s").replace("ż", "z").replace("é", "e")
         for ch in newline:
-            if ord(ch) in range(97,123):
-                output+=ch
+            if ord(ch) in range(97, 123):
+                output += ch
     return output
 
-def counting(text,length):
-    letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-             'v', 'w','x', 'y', 'z']
-    freq={}
+
+def counting(text, length):
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z']
+    freq = {}
     for x in range(length):
-        freq[x]={}
+        freq[x] = {}
         for y in letters:
-            freq[x][y]=0
-    index=0
+            freq[x][y] = 0
+    index = 0
     for ch in text:
-        freq[index%length][ch]+=1
-        index+=1
+        freq[index % length][ch] += 1
+        index += 1
     return freq
-if __name__=="__main__":
-    print(counting(simplify("C:/pt.txt"),5))
+
+
+if __name__ == "__main__":
+    a = counting(simplify("pt.txt"), 5)
+    for x in a.keys():
+        print(a[x])
+        for y in a[x].keys():
+            print(a[x][y])
--- vigenere.py
+++ vigenere.py
@@ -1,10 +1,26 @@
 alph_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
            'v', 'w', 'x', 'y', 'z']
-def unpol(text):
+
+
+def vig_ciph(text, enc_key):
     output = ""
-    for line in open(text,"r", encoding="utf8").readlines():
-        newline=replace("ą","a").replace("ć","c").replace("ę","e").replace("ł","l").replace("ń","n").replace("ó","o").replace("ś","s").replace("ż","z")
+    n = 0
+    enc_key = enc_key.lower()
+    for line in open(text, "r", encoding="utf8").readlines():
+        newline = line.replace("ą", "a").replace("ć", "c").replace("ę", "e").replace("ł", "l").replace(
+            "ń","n").replace("ó","o").replace("ś", "s").replace("ż", "z").replace("Ć", "C").replace(
+            "Ł", "L").replace("Ó", "O").replace("Ś", "S").replace("Ż", "Z").replace("Ź", "Z").replace("é", "e")
         for ch in newline:
-            if ord(ch) in range(97,123):
-                output+=ch
+            if ord(ch) in range(97, 123):
+                output += chr((ord(enc_key[n % len(enc_key)]) - 97 + ord(ch) - 97) % len(alph_en) + 97)
+                n += 1
+            elif ord(ch) in range(65, 90):
+                output += chr((ord(enc_key[n % len(enc_key)]) - 97 + ord(ch) - 65) % len(alph_en) + 65)
+                n += 1
+            else:
+                output += ch
     return output
+
+
+if __name__ == "__main__":
+    print(vig_ciph("test.txt", "key"))
